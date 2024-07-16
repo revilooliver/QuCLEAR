47a50a0e3809e8d7cc33aed69c4a605dd4e369d3
alvinquantum <agonzales@anl.gov>
2024-07-16 18:53:10 -0500

    adding labs benchmarks
    based on qokit

---
+++ benchmarks/labs_benchmarks.py
@@ -0,0 +1,38 @@
+import json
+from qokit.labs import get_gate_optimized_terms_greedy
+
+
+def paulis_mixer(num_qubits):
+    '''Returns the list of Pauli strings for the mixer Hamiltonian.'''
+    hamiltonian=[]
+    for idx in range(num_qubits):
+        pauli_str=list("I"*num_qubits)
+        pauli_str[idx]="X"
+        hamiltonian.append("".join(pauli_str))
+    return hamiltonian
+
+def labs_pauli_layers(num_qubits, layers):
+    '''Returns the list of cost Hamiltonian followed by the mixer Hamiltonian repeated
+    for the desired layers.
+    Args:
+        num_qubits: number of qubits in circuit.
+        totter_number: number of QAOA layers.'''
+    terms = get_gate_optimized_terms_greedy(num_qubits) # indices of Pauli Zs
+    print(f"terms {terms}")
+    cost_hamiltonian=[]
+    for t in terms:
+        pauli_str=list("I"*num_qubits)
+        for idx in t:
+            pauli_str[idx]="Z"
+        cost_hamiltonian.append("".join(pauli_str))
+    return [cost_hamiltonian, paulis_mixer(num_qubits)]*layers
+    
+
+def save_to_json(data, file=None):
+    if file==None:
+        file="labs_paulis/labs_paulis.json"
+    else:
+        file="labs_paulis/"+file
+
+    with open(file, "w") as f:
+        json.dump(data, f)
